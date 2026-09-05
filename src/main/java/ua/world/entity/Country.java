a15093dce053663be1c0dda7bd12c95dc7069b88
Artem <26and06@gmail.com>
2026-09-05 12:15:04 +0300

    added dependecies

--- src/main/java/ua/world/entity/Country.java
+++ src/main/java/ua/world/entity/Country.java
@@ -1,9 +1,14 @@
 package ua.world.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(schema = "world", name = "country")
 public class Country {
+    @Id
+    private Long id;
+
+
 }
