2435cec3b148620f2ab409c15aa75598488e9934
Artem <26and06@gmail.com>
2026-09-07 14:28:25 +0300

    created DataTransformer and modified Main for testing

--- src/main/java/ua/world/Main.java
+++ src/main/java/ua/world/Main.java
@@ -123,33 +123,7 @@ public class Main {
     }
 
     private List<CityCountry> transformData(List<City> cities) {
-        return cities.stream().map(city -> {
-            CityCountry res = new CityCountry();
-            res.setId(city.getId());
-            res.setName(city.getName());
-            res.setPopulation(city.getPopulation());
-            res.setDistrict(city.getDistrict());
-
-            Country country = city.getCountry();
-            res.setAlternativeCountryCode(country.getAlternativeCode());
-            res.setContinent(country.getContinent());
-            res.setCountryCode(country.getCode());
-            res.setCountryName(country.getName());
-            res.setCountryPopulation(country.getPopulation());
-            res.setCountryRegion(country.getRegion());
-            res.setCountrySurfaceArea(country.getSurfaceArea());
-            Set<CountryLanguage> countryLanguages = country.getLanguages();
-            Set<Language> languages = countryLanguages.stream().map(cl -> {
-                Language language = new Language();
-                language.setLanguage(cl.getLanguage());
-                language.setOfficial(cl.getOfficial());
-                language.setPercentage(cl.getPercentage());
-                return language;
-            }).collect(Collectors.toSet());
-            res.setLanguages(languages);
-
-            return res;
-        }).collect(Collectors.toList());
+        return new DataTransformer().transform(cities);
     }
     private void pushToRedis(List<CityCountry> data) {
         try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
